48486705afdcb350a6411543da8c0a54a8cb3cfb
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-26 20:07:06 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(name)
+    @persons_name = name
+  end
+
+  def name
+    @persons_name
+  end
+
+  def jog=(job)
+    @persons_job = job
+  end
+
+  def breed
+    @persons_job
+  end
+  
+end
